f35b211b409a2ae9e31cd893cfc2a2b502c04899
Wayne <39188178+silent717120@users.noreply.github.com>
2020-05-19 17:59:29 +0800

    Add files via upload

---
+++ CSPDFConvertor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GhostscriptSharp;
+
+public class CSPDFConvertor : MonoBehaviour
+{
+    public static CSPDFConvertor instance;
+    // Use this for initialization
+    void Start()
+    {
+        instance = this;
+    }
+
+    /// <summary>
+    /// convert all pdf to jpeg
+    /// </summary>
+    //用來轉換PDF成一張一張的jpeg圖片
+    public void f_ConvertAllPDF(string pdfPath, string savePath)
+    {
+        GhostscriptSettings ghostscriptSettings = new GhostscriptSettings();
+        ghostscriptSettings.Device = GhostscriptSharp.Settings.GhostscriptDevices.jpeg;
+        ghostscriptSettings.Size.Native = GhostscriptSharp.Settings.GhostscriptPageSizes.legal; //legal為原尺寸
+        //ghostscriptSettings.Size.Manual = new System.Drawing.Size(2552, 3579);
+        ghostscriptSettings.Resolution = new System.Drawing.Size(150, 150);
+        ghostscriptSettings.Page.AllPages = true;
+        GhostscriptWrapper.GenerateOutput(pdfPath, savePath, ghostscriptSettings);
+
+        Debug.Log("Complete!");
+    }
+}
